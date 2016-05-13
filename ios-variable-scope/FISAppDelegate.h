6e10783500f8ad2fcb4482b6ac4756bb392882e0
TiciaChance <laticia.chance@flatironschool.com>
2016-05-13 11:46:35 -0400

    Done.

--- ios-variable-scope/FISAppDelegate.h
+++ ios-variable-scope/FISAppDelegate.h
@@ -12,10 +12,15 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+// returning a mutable array, but will be adding a string to the array through the method arrayByAddingString
+
 -(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array;
+
+// method that will be returning the number of strings that are upper case in the array
 
 -(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array;
 
+//I'm assuming this pretty much does what the method says 
 -(void)removeAllObjectsFromArray:(NSMutableArray *)array;
 
 @end
--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,41 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+// returning a mutable array, but will be adding a string to the array through the method arrayByAddingString
 
-@end
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array; {
+    
+//  return string with array added to it - cannot modify the original array --> create copy of the argument array(arrayWithArray)
+    
+   NSMutableArray *newArray =  [array mutableCopy];
+    
+    [newArray addObject:string];
+    
+    return newArray;
+    
+}
+
+// method that will be returning the number of strings that are upper case in the array
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array; {
+    
+    NSUInteger countForUpcaseArray = 0;
+    
+    for(NSUInteger i = 0; i < [array count]; i++ ) {
+        
+        if ([array[i] isEqualToString: [array[i] uppercaseString]]) {
+            
+            countForUpcaseArray++;
+        }
+        
+    }
+    
+    return countForUpcaseArray;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array {
+    
+    [array removeAllObjects];
+}
+
+    @end
